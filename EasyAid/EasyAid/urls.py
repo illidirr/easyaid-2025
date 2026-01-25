271b537d1b6444700c5184a21326722ec78848c9
illidirr <zhdanko.y@bk.ru>
2026-01-25 23:52:03 +0300

    Connect main app to root URL

--- EasyAid/EasyAid/urls.py
+++ EasyAid/EasyAid/urls.py
@@ -5,13 +5,13 @@ from django.conf.urls.static import static
 from django.http import HttpResponse
 from django.views.generic import TemplateView
 
-def tezt(request):
-    return HttpResponse("Django is working!")
 
+def tezt(request):
+    return render(request, 'main/index.html')
 urlpatterns = [
-    path('', tezt),
+    path('',  include('main.urls')),
     path('admin/', admin.site.urls),
-   # path('', TemplateView.as_view(template_name='index.html'), name='home'),
+    path('', TemplateView.as_view(template_name='index.html'), name='home'),
 ]
 if settings.DEBUG:
     urlpatterns += static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
