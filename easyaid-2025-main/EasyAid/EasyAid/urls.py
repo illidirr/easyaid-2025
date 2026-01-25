fe39187850633e1e6f96cc7d290aaf8ed265195a
illidirr <zhdanko.y@bk.ru>
2026-01-25 17:40:54 +0300

    Initial commit for Render deployment

---
+++ easyaid-2025-main/EasyAid/EasyAid/urls.py
@@ -0,0 +1,12 @@
+from django.contrib import admin
+from django.urls import path, include
+from django.conf import settings
+from django.conf.urls.static import static
+
+urlpatterns = [
+    path('admin/', admin.site.urls),
+    path('', include('main.urls')),
+    path('', TemplateView.as_view(template_name='index.html'), name='home'),
+]
+if settings.DEBUG:
+    urlpatterns += static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
