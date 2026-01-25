b48f41cc105f6521e4ceae51536bea4429990ffb
illidirr <zhdanko.y@bk.ru>
2026-01-25 19:28:31 +0300

    Add railpack.json and update settings

--- easyaid-2025-main/EasyAid/main/admin.py
+++
@@ -1,8 +0,0 @@
-from django.contrib import admin
-from .models import Video
-
-@admin.register(Video)
-class VideoAdmin(admin.ModelAdmin):
-    list_display = ['title', 'created_at']
-    list_filter = ['created_at']
-    search_fields = ['title', 'description']
