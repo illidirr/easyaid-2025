012802aec0965ea082259402e9987076d2d2165e
illidirr <zhdanko.y@bk.ru>
2026-01-25 23:25:27 +0300

    Fix urls.py duplicate paths

--- EasyAid/EasyAid/urls.py
+++ EasyAid/EasyAid/urls.py
@@ -3,6 +3,7 @@ from django.urls import path, include
 from django.conf import settings
 from django.conf.urls.static import static
 from django.http import HttpResponse
+from django.views.generic import TemplateView
 
 def tezt(request):
     return HttpResponse("Django is working!")
@@ -10,8 +11,7 @@ def tezt(request):
 urlpatterns = [
     path('', tezt),
     path('admin/', admin.site.urls),
-    path('', include('main.urls')),
-    path('', TemplateView.as_view(template_name='index.html'), name='home'),
+   # path('', TemplateView.as_view(template_name='index.html'), name='home'),
 ]
 if settings.DEBUG:
     urlpatterns += static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
