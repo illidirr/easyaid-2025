42ca7d7a26d0a9984df4688f1cc1a6096a7f7f6a
illidirr <zhdanko.y@bk.ru>
2026-01-25 19:39:49 +0300

    Fix wsgi module path

--- EasyAid/start.sh
+++ EasyAid/start.sh
@@ -9,5 +9,5 @@ python manage.py collectstatic --noinput
 gunicorn EasyAid.wsgi:application --bind 0.0.0.0:$PORT
 {
   "build": "pip install -r requirements.txt && python manage.py collectstatic --noinput",
-  "start": "gunicorn EasyAid.wsgi:application --bind 0.0.0.0:$PORT"
+  "start": "gunicorn EasyAid.EasyAid.wsgi:application --bind 0.0.0.0:$PORT"
 }
