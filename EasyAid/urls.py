07c055bb70fff6d76222f3f0e07323b610114d29
illidirr <zhdanko.y@bk.ru>
2026-01-27 18:18:14 +0300

    Add health check endpoint

--- EasyAid/urls.py
+++ EasyAid/urls.py
@@ -10,7 +10,10 @@ def index_view(request):
 
 urlpatterns = [
     path('admin/', admin.site.urls),
-    path('', include('main.urls')),  # Главные URL из приложения main
+    path('', include('main.urls')),
+    path('health/', health_check),
+    path('health/', views.health_check, name='health_check'),
+    # Главные URL из приложения main
 ]
 
 # В режиме DEBUG добавляем обслуживание медиафайлов
