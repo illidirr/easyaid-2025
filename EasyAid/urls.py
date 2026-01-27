90e794cc069c3e6b156ec2578556c537225d437c
illidirr <zhdanko.y@bk.ru>
2026-01-28 00:19:34 +0300

    Fix settings import in urls.py and add media serving for DEBUG mode

--- EasyAid/urls.py
+++ EasyAid/urls.py
@@ -3,7 +3,7 @@ from django.contrib import admin
 from django.urls import path, include
 from django.shortcuts import render
 from main import views
-
+from django.conf import settings
 # ДОБАВЬТЕ ЭТУ СТРОКУ
 
 # Создайте функции прямо здесь
